ebdb00ac20cee6206b59b7059c3c5c29b40118fd
David Parker <dav1dp0101@1022606.no-reply.drupal.org>
2022-12-28 14:39:13 -0500

    Standardizing and prettifying player

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,12 +22,17 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
+// Set some global variables for the audio player
+let savedVolume = 1.0;
+let muted = false;
+
 let Hooks = {}
 
 Hooks.AudioPlayer = {
     mounted(){
         this.player = this.el.querySelector("audio");
         this.player.src = "https://audio.cfrc.ca/stream";
+        this.player.volume = 1.0;
         let enableAudio = () => {
             if(this.player.src){
               document.removeEventListener("click", enableAudio)
@@ -46,7 +51,18 @@ Hooks.AudioPlayer = {
             }
         })
 
-        
+        this.el.addEventListener("js:volume_up", () =>{
+            this.adjustVol("up");
+        })
+        this.el.addEventListener("js:volume_down", () =>{
+            this.adjustVol("down");
+        })
+        this.el.addEventListener("js:volume_off", () =>{
+            this.adjustVol("off");
+        })
+        this.el.addEventListener("js:volume_on", () =>{
+            this.adjustVol("on");
+        })
 
         this.handleEvent("play", () => {
             this.play({sync: true});
@@ -55,7 +71,54 @@ Hooks.AudioPlayer = {
         this.handleEvent("stop", () => this.stop());
     },
 
-    play(){
+    adjustVol(direction) {
+        let volume = this.player.volume;
+        //let savedVolume = getSavedVolume();
+        switch (direction) {
+            case "up":
+                 if (volume == 0.0) {
+                    this.player.volume = 0.1;
+                } else if (volume > 0.9) {
+                    this.player.volume = 1.0;
+                }else if (volume < 1.0) {
+                    this.player.volume = volume + 0.1;
+                }
+                savedVolume = this.player.volume;
+                break;
+            case "down":
+                if (volume == 1.0) {
+                    this.player.volume = 0.9;
+                }
+                else if (volume < 0.14) {
+                    this.player.volume = 0.0;
+                }
+                else {
+                    this.player.volume = volume - 0.1;
+                }
+                savedVolume = this.player.volume;
+                break;
+            case "off":
+                if (!muted) {
+                    muted = true;
+                    this.player.volume = 0.0;
+                }
+                break;
+            case "on":
+                if (muted) {
+                    muted = false;
+                    this.player.volume = savedVolume;
+                }
+                break;
+            default: 
+                console.log("Error: Default case reached.");
+                break;
+        }
+        volume = this.player.volume;
+        //console.log("Volume: ", this.player.volume);
+        //console.log("Saved Volume: ", savedVolume);
+    },
+
+    play () {
         this.player.play();
     },
 
@@ -65,8 +128,50 @@ Hooks.AudioPlayer = {
 
     stop(){
         this.player.pause();
-    }
+    }, 
 }
+// Set global variable for volume input range slider
+const volume = document.querySelector("#volume");
+const muteControl = document.querySelector("#muteControl");
+let isMuted = false;
+
+function unMute() {
+    isMuted = false;
+        muteControl.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-volume-up-fill" viewBox="0 0 16 16">
+            <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
+            <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
+            <path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"/>
+        </svg>
+    `;
+}
+
+volume.addEventListener("input", () => {
+    music.volume = volume.value; 
+    savedVolume = volume.value;
+    if (isMuted) {
+         unMute();
+    }
+});
+
+muteControl.addEventListener("click", () => {
+    if (isMuted) {
+      // Unmute the stream
+      music.volume = savedVolume;
+      volume.value = savedVolume;
+      unMute();
+    } else {
+        // Mute the stream
+        isMuted = true;
+        music.volume = 0.0;
+        volume.value = 0.0;
+        muteControl.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-volume-mute-fill" viewBox="0 0 16 16">
+                <path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/>
+            </svg>
+        `;
+    }
+});
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
--- lib/audio_player_web/live/player_live.ex
+++ lib/audio_player_web/live/player_live.ex
@@ -38,9 +38,15 @@ defmodule AudioPlayerWeb.PlayerLive do
     {:noreply, socket}
   end
 
+  def handle_event("volume_change", _, socket) do
+
+    JS.dispatch("")
+
+    {:noreply, socket}
+  end
+
   def handle_event("play_pause", _, socket) do
     %{playing: playing} = socket.assigns
-    js_play_pause()
 
     cond do
       playing ->
@@ -59,94 +65,145 @@ defmodule AudioPlayerWeb.PlayerLive do
     |> JS.dispatch("js:play_pause", to: "#audio-player")
   end
 
-  defp js_adjust_volume(volume) do
+  defp js_adjust_volume(:down) do
+    JS.dispatch("js:volume_down", to: "#audio-player")
+  end
 
+  defp js_adjust_volume(:up) do
+    JS.dispatch("js:volume_up", to: "#audio-player")
+  end
+
+  defp js_adjust_volume(:off) do
+    JS.dispatch("js:volume_off", to: "#audio-player")
+  end
+
+  defp js_adjust_volume(:on) do
+    JS.dispatch("js:volume_on", to: "#audio-player")
   end
 
   def render(assigns) do
     ~H"""
-    <h1>Audio Player</h1>
+    <%!-- <h1>Audio Player</h1> --%>
     <div id="audio-player" phx-hook="AudioPlayer" class="w-full" role="region" aria-label="Player">
       <div id="audio-ignore" phx-update="ignore">
-        <audio></audio>
+        <audio id="music"></audio>
       </div>
-      <!-- play/pause -->
+
+      <div class="first-row row">
+        <!-- play/pause -->
         <button
-            type="button"
-            class="mx-auto scale-75"
-            phx-click={js_play_pause()}
-            aria-label={
-              if @playing do
-                "Pause"
-              else
-                "Play"
-              end
-            }
-          >
-            <%= if @playing do %>
-              <svg id="player-pause" width="50" height="50" fill="none">
-                <circle
-                  class="text-gray-300 dark:text-gray-500"
-                  cx="25"
-                  cy="25"
-                  r="24"
-                  stroke="currentColor"
-                  stroke-width="1.5"
-                />
-                <path d="M18 16h4v18h-4V16zM28 16h4v18h-4z" fill="currentColor" />
-              </svg>
-            <% else %>
-              <svg
-                id="player-play"
-                width="50"
-                height="50"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
+          type="button"
+          class="mx-auto"
+          phx-click={js_play_pause()}
+          aria-label={
+            if @playing do
+              "Pause"
+            else
+              "Play"
+            end
+          }
+        >
+          <%= if @playing do %>
+            <svg id="player-pause" width="50" height="50" fill="none">
+              <circle
+                class="text-black dark:text-gray-500"
+                cx="25"
+                cy="25"
+                r="24"
                 stroke="currentColor"
-              >
-                <circle
-                  id="svg_1"
-                  stroke-width="0.8"
-                  stroke="currentColor"
-                  r="11.4"
-                  cy="12"
-                  cx="12"
-                  class="text-gray-300 dark:text-gray-500"
-                />
-                <path
-                  stroke="null"
-                  fill="currentColor"
-                  transform="rotate(90 12.8947 12.3097)"
-                  id="svg_6"
-                  d="m9.40275,15.10014l3.49194,-5.58088l3.49197,5.58088l-6.98391,0z"
-                  stroke-width="1.5"
-                  fill="none"
-                />
+                stroke-width="1.5"
+              />
+              <path d="M18 16h4v18h-4V16zM28 16h4v18h-4z" fill="currentColor" />
+            </svg>
+          <% else %>
+            <svg
+              id="player-play"
+              width="50"
+              height="50"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <circle
+                id="svg_1"
+                stroke-width="0.8"
+                stroke="currentColor"
+                r="11.4"
+                cy="12"
+                cx="12"
+                class="text-black dark:text-gray-500"
+              />
+              <path
+                stroke="null"
+                fill="currentColor"
+                transform="rotate(90 12.8947 12.3097)"
+                id="svg_6"
+                d="m9.40275,15.10014l3.49194,-5.58088l3.49197,5.58088l-6.98391,0z"
+                stroke-width="1.5"
+                fill="none"
+              />
+            </svg>
+          <% end %>
+        </button>
+        <!-- /play/pause -->
+        <div class="pop-container">
+          <a href="./player"
+            onclick="window.open(this.href,'targetWindow',
+              `toolbar=no,
+                location=no,
+                status=no,
+                menubar=no,
+                scrollbars=yes,
+                resizable=yes,
+                width=550,
+                height=200`);
+              return false;"
+          >
+            <span id="player-pop-up" class="player-button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+                <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
               </svg>
-            <% end %>
-          </button>
-          <!-- /play/pause -->
-    </div>
-    <div class="meter">
-      <span style={"width: #{Float.round(@volume * 100, 1)}%"}>
-        <%= round(@volume * 100) / 100 %>
-      </span>
-    </div>
-    <button phx-click="mute">
-      Mute
-    </button>
+            </span>
+          </a>
+        </div>
+      </div>
 
-    <button phx-click="unmute">
-      Unmute
-    </button>
-    <button phx-click="down">
-      Down
-    </button>
-
-    <button phx-click="up">
-      Up
-    </button>
+      <div class="second-row row">
+        <!-- <span id="current-time" class="time">0:00</span>
+        <input type="range" id="seek-slider" max="100" value="0">
+        <span id="duration" class="time">0:00</span> -->
+        <label id="muteControl" class="volumeControl volumeLabel player-button" for="volume">
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-volume-up-fill" viewBox="0 0 16 16">
+              <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
+              <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
+              <path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"/>
+            </svg>
+        </label>
+        <input
+          type="range"
+          name="volume"
+          id="volume"
+          min="0.0"
+          max="1.0"
+          step="0.1"
+          value="1.0"
+        />
+      </div>
+      <!-- <button phx-click={js_adjust_volume(:off)}>
+        Mute
+      </button>
+      <button phx-click={js_adjust_volume(:on)}>
+        Unmute
+      </button> -->
+      <!-- <button phx-click={js_adjust_volume(:down)}>
+        Down
+      </button>
+      <button phx-click={js_adjust_volume(:up)}>
+        Up
+      </button> -->
+    </div>
     """
   end
 
